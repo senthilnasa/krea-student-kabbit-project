acccf5c06b552d24456d379a906032eba83d74d2
senthilnasa <senthilprabu005@gmail.com>
2024-04-13 18:06:49 +0530

    Update configuration and email credentials in config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', 'secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://user:password@localhost/cab_sharing')
+    SECRET_KEY = os.getenv('FLASK_SECRET_KEY', '333')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://kabbit:kabbit@localhost:3306/kabbit_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME',"kabbitkrea@gmail.com")
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD',"rktkonpdyjuyccxj")
